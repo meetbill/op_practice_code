766579523961f85cf34f60d85b6a0fe53a6f4ade
BillWang139967 <wangbin139967@163.com>
2016-11-25 18:38:00 +0800

    add:python-pythonstartup

---
+++ Linux/python/pythonstartup.sh
@@ -0,0 +1,22 @@
+#########################################################################
+# File Name: pythonstartup.sh
+# Author: 遇见王斌
+# mail: meetbill@163.com
+# Created Time: 2016-11-25 16:10:04
+#########################################################################
+#!/bin/bash
+
+echo "import readline, rlcompleter" > ~/.pythonstartup.py
+echo 'readline.parse_and_bind("tab: complete")' >> ~/.pythonstartup.py
+
+
+
+CK_BASH=`grep "PYTHONSTARTUP" ~/.bashrc | wc -l`
+if [ "w${CK_BASH}" = "w0" ]
+then
+	echo " " >> ~/.bashrc
+	echo "export PYTHONSTARTUP=~/.pythonstartup.py" >> ~/.bashrc
+fi 
+. ~/.bashrc
+
+echo ":)"
